f9cc2d473480e55fde1e366af1f3ecd84cf80de6
Xiuheng <wuxhme@gmail.com>
2023-08-24 16:59:49 +0800

    Also use randomly generated names

--- src/main/java/xyz/facta/jtools/genmutator/util/VarNameGenerator.java
+++ src/main/java/xyz/facta/jtools/genmutator/util/VarNameGenerator.java
@@ -30,29 +30,48 @@ public class VarNameGenerator {
 
     private static final Random rand = new Random();
     private static final Set<String> generatedNames = new HashSet<>();
+    private static final int MAX_TRIES = 4;
 
     public static String generateVariableName() {
+        int tries = 1;
         String name;
         do {
-            StringBuilder nameBuilder = new StringBuilder();
+            if (tries <= MAX_TRIES) {
+                StringBuilder nameBuilder = new StringBuilder();
 
-            // 50% chance to add a prefix
-            if (rand.nextInt(100) < 50) {
-                nameBuilder.append(PREFIXES[rand.nextInt(PREFIXES.length)]);
+                // 50% chance to add a prefix
+                if (rand.nextInt(100) < 50) {
+                    nameBuilder.append(PREFIXES[rand.nextInt(PREFIXES.length)]);
+                }
+
+                // 50% chance to add an adjective
+                if (rand.nextInt(100) < 50) {
+                    nameBuilder.append(capitalize(ADJECTIVES[rand.nextInt(ADJECTIVES.length)]));
+                }
+
+                // Always add a main word
+                nameBuilder.append(capitalize(WORD_POOL[rand.nextInt(WORD_POOL.length)]));
+                name = decapitalize(nameBuilder.toString());
+                tries++;
+            } else {
+                name = generateRandomLetterName();
             }
-
-            // 50% chance to add an adjective
-            if (rand.nextInt(100) < 50) {
-                nameBuilder.append(capitalize(ADJECTIVES[rand.nextInt(ADJECTIVES.length)]));
-            }
-
-            // Always add a main word
-            nameBuilder.append(capitalize(WORD_POOL[rand.nextInt(WORD_POOL.length)]));
-            name = decapitalize(nameBuilder.toString());
         } while (generatedNames.contains(name));
 
         generatedNames.add(name);
         return name;
+    }
+
+    private static String generateRandomLetterName() {
+        int length = 5 + rand.nextInt(5);  // generate names between 5 and 9 characters
+        StringBuilder name = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            char c = (char) ('a' + rand.nextInt(26));  // generate random lowercase letters
+            name.append(i == 0 ? Character.toUpperCase(c) : c);  // capitalize the first letter
+        }
+
+        return name.toString();
     }
 
     private static String capitalize(String str) {
