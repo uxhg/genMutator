48a8208d87d1f6c5bafe3fb28235db753a646602
Xiuheng <wuxhme@gmail.com>
2023-08-25 13:53:29 +0800

    Mutate name with prob

--- src/main/java/xyz/facta/jtools/genmutator/mut/VarRenameMutator.java
+++ src/main/java/xyz/facta/jtools/genmutator/mut/VarRenameMutator.java
@@ -5,12 +5,23 @@ import spoon.refactoring.CtRenameGenericVariableRefactoring;
 import spoon.reflect.declaration.CtVariable;
 import xyz.facta.jtools.genmutator.util.VarNameGenerator;
 
+import java.util.Random;
+
 public class VarRenameMutator extends AbstractProcessor<CtVariable> {
 
+    private static final float MUTATION_PROBABILITY = 0.5f;  // 50% chance
+    private final Random random = new Random();
 
     @Override
     public void process(CtVariable ctVar) {
-        String newName = VarNameGenerator.generateVariableName();
-        new CtRenameGenericVariableRefactoring().setTarget(ctVar).setNewName(newName).refactor();
+        if (shouldMutate()) {
+            String newName = VarNameGenerator.generateVariableName();
+            new CtRenameGenericVariableRefactoring().setTarget(ctVar).setNewName(newName).refactor();
+        }
     }
+
+    private boolean shouldMutate() {
+        return random.nextFloat() <= MUTATION_PROBABILITY;
+    }
+
 }
